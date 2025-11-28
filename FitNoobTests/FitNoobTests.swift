370426a2be4013a542d0f31614658c1ba5be4b2b
Tharuka Soooriyaarachchi <tharukaravisara@gmail.com>
2025-11-28 22:02:05 +0530

    Initial Commit

---
+++ FitNoobTests/FitNoobTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitNoobTests.swift
+//  FitNoobTests
+//
+//  Created by Tharuka Sooriyaarachchi on 11/28/25.
+//
+
+import Testing
+@testable import FitNoob
+
+struct FitNoobTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
